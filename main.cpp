56b1091cd3b84452f118376d9db9208f8b97f3f0
unknown <p.angelov99@gmail.com>
2019-06-06 23:07:19 +0300

    final update

--- main.cpp
+++ main.cpp
@@ -1,71 +1,68 @@
-<<<<<<< HEAD
-#include <iostream>
-#include "automaton.h"
-
-using namespace std;
-
-int main()
-{
-    Automaton a;
-    a.openFromFile("automatas/automata1.txt");
-    a.printTransitions();
-
-    cout << a.determined() << endl;
-    cout << a.isEmpty() << endl;
-
-    a.writeInFile("automatas/automata2.txt");
-
-    a.removeUnreachableStates();
-
-    a.writeInFile("automatas/automata2_reachable.txt");
-
-    Automaton b = a.Union(a);
-    b.writeInFile("automatas/automata3.txt");
-
-    Automaton con = a.Concat(a);
-    con.writeInFile("automatas/automata_concat.txt");
-
-    cout << "is aba recognized?: " << a.recognizeWord(a.getStartState(), "aba", 0) << "\n";
-
-    cout << "is bab recognized?: " << a.recognizeWord(a.getStartState(), "bab", 0) << "\n";
-
-    Automaton un = a.positiveClosure();
-    un.writeInFile("automatas/automata_posClos.txt");
-
-    Automaton automatonForA = automatonForSymbol('a');
-    automatonForA.writeInFile("automatas/automatonForA.txt");
-
-    Automaton automatonClosure = a.Closure();
-    automatonClosure.writeInFile("automatas/automatonClosure.txt");
-
-    automatonClosure.removeUnreachableStates();
-    automatonClosure.normalize();
-    automatonClosure.writeInFile("automatas/automatonClosureReachable.txt");
-
-    automatonClosure.removeEpsilonTransitions();
-    automatonClosure.writeInFile("automatas/automatonClosureNoEps.txt");
-
-    Automaton automatonRegex = Regex("(a+b).c");
-    automatonRegex.writeInFile("automatas/automatonRegex.txt");
-
-    automatonRegex.removeEpsilonTransitions();
-    automatonRegex.writeInFile("automatas/automatonRegexNoEps.txt");
-
-    automatonRegex.removeUnreachableStates();
-    automatonRegex.normalize();
-    automatonRegex.writeInFile("automatas/automatonRegexNorm.txt");
-
-
-
-	return 0;
-}
-=======
 #include <iostream>
 #include "automaton.h"
 #include <cstring>
 
 using namespace std;
 
+
+bool checkBrackets(char *text)
+{
+    int numBrackets = 0;
+    int len = strlen(text);
+    for(int i = 0; i < len; i++)
+    {
+        if(text[i] == '(')
+        {
+            numBrackets ++;
+        }
+        if(text[i] == ')')
+        {
+            numBrackets --;
+        }
+        if(numBrackets < 0)
+            return false;
+    }
+    return numBrackets == 0;
+}
+
+void findWord(char text[], char word[], int& index)
+{
+    int len = strlen(text);
+
+    int cnt = 0;
+
+    for(; index < len; index++)
+    {
+        if(text[index] == ' ')
+        {
+            index ++;
+            word[cnt] = '\0';
+            return;
+        }
+        word[cnt] = text[index];
+        cnt ++;
+    }
+    word[cnt] = '\0';
+    return;
+}
+
+int castToInt(char text[])
+{
+    int num = -1;
+    int len = strlen(text);
+    for(int i = 0; i < len; i++)
+    {
+        if(i == 0)
+            num = 0;
+        if(text[i] < '0' || text[i] > '9')
+        {
+            return -1;
+        }
+        num *= 10;
+        num += text[i] - '0';
+    }
+    return num;
+}
 
 int main()
 {
@@ -74,19 +71,25 @@ int main()
     while(!isClosed)
     {
 
-        string command;
-        cin >> command;
+        char commandLine[256];
+        cin.getline(commandLine, 256);
 
-        if(command == "exit")
+        int index = 0;
+        char command[256];
+        findWord(commandLine, command, index);
+
+        if(!strcmp(command, "exit"))
         {
             isClosed = true;
             continue;
         }
 
-        if(command == "open")
+        if(!strcmp(command, "open"))
         {
             char filepath[256];
-            cin >> filepath;
+
+            findWord(commandLine, filepath, index);
+
             automata.push_back(Automaton());
 
             automata[automata.size() - 1].openFromFile(filepath);
@@ -96,7 +99,7 @@ int main()
 
         }
 
-        if(command == "list")
+        if(!strcmp(command, "list"))
         {
             for(int i = 0; i < automata.size(); i++)
             {
@@ -106,10 +109,15 @@ int main()
             continue;
         }
 
-        if(command == "print")
+        if(!strcmp(command, "print"))
         {
             int automatonID;
-            cin >> automatonID;
+
+            char idChar[256];
+
+            findWord(commandLine, idChar, index);
+
+            automatonID = castToInt(idChar);
 
             if(automatonID < 0 || automatonID >= automata.size())
             {
@@ -122,12 +130,17 @@ int main()
             continue;
         }
 
-        if(command == "save")
+        if(!strcmp(command, "save"))
         {
             int automatonID;
             char filepath[256];
+            char idChar[256];
 
-            cin >> automatonID >> filepath;
+            findWord(commandLine, idChar, index);
+
+            findWord(commandLine, filepath, index);
+
+            automatonID = castToInt(idChar);
 
             if(automatonID < 0 || automatonID >= automata.size())
             {
@@ -140,10 +153,13 @@ int main()
             continue;
         }
 
-        if(command == "empty")
+        if(!strcmp(command, "empty"))
         {
             int automatonID;
-            cin >> automatonID;
+            char idChar[256];
+
+            findWord(commandLine, idChar, index);
+            automatonID = castToInt(idChar);
 
             if(automatonID < 0 || automatonID >= automata.size())
             {
@@ -163,10 +179,13 @@ int main()
             continue;
         }
 
-        if(command == "deterministic")
+        if(!strcmp(command, "deterministic"))
         {
             int automatonID;
-            cin >> automatonID;
+            char idChar[256];
+
+            findWord(commandLine, idChar, index);
+            automatonID = castToInt(idChar);
 
             if(automatonID < 0 || automatonID >= automata.size())
             {
@@ -186,12 +205,16 @@ int main()
             continue;
         }
 
-        if(command == "recognize")
+        if(!strcmp(command, "recognize"))
         {
             int automatonID;
             char word[256];
+            char idChar[256];
 
-            cin >> automatonID >> word;
+            findWord(commandLine, idChar, index);
+            findWord(commandLine, word, index);
+
+            automatonID = castToInt(idChar);
 
             if(automatonID < 0 || automatonID >= automata.size())
             {
@@ -215,10 +238,17 @@ int main()
             continue;
         }
 
-        if(command == "union")
+        if(!strcmp(command, "union"))
         {
             int automatonID1, automatonID2;
-            cin >> automatonID1 >> automatonID2;
+
+            char idChar1[256], idChar2[256];
+
+            findWord(commandLine, idChar1, index);
+            findWord(commandLine, idChar2, index);
+
+            automatonID1 = castToInt(idChar1);
+            automatonID2 = castToInt(idChar2);
 
             if(automatonID1 < 0 || automatonID1 >= automata.size())
             {
@@ -241,10 +271,16 @@ int main()
             continue;
         }
 
-        if(command == "concat")
+        if(!strcmp(command, "concat"))
         {
             int automatonID1, automatonID2;
-            cin >> automatonID1 >> automatonID2;
+            char idChar1[256], idChar2[256];
+
+            findWord(commandLine, idChar1, index);
+            findWord(commandLine, idChar2, index);
+
+            automatonID1 = castToInt(idChar1);
+            automatonID2 = castToInt(idChar2);
 
             if(automatonID1 < 0 || automatonID1 >= automata.size())
             {
@@ -267,10 +303,12 @@ int main()
             continue;
         }
 
-        if(command == "union")
+        if(!strcmp(command, "un"))
         {
             int automatonID;
-            cin >> automatonID;
+            char idChar[256];
+            findWord(commandLine, idChar, index);
+            automatonID = castToInt(idChar);
 
             if(automatonID < 0 || automatonID >= automata.size())
             {
@@ -287,10 +325,16 @@ int main()
             continue;
         }
 
-        if(command == "regex")
+        if(!strcmp(command, "regex"))
         {
             char _regex[256];
-            cin >> _regex;
+            findWord(commandLine, _regex, index);
+
+            if(!checkBrackets(_regex))
+            {
+                cout << "Invalid regex\n";
+                continue;
+            }
 
             automata.push_back(Regex(_regex));
 
@@ -301,10 +345,12 @@ int main()
             continue;
         }
 
-        if(command == "determine")
+        if(!strcmp(command, "determine"))
         {
             int automatonID;
-            cin >> automatonID;
+            char idChar[256];
+            findWord(commandLine, idChar, index);
+            automatonID = castToInt(idChar);
 
             if(automatonID < 0 || automatonID >= automata.size())
             {
@@ -400,4 +446,3 @@ int main()
 
 	return 0;
 }
->>>>>>> yeah
--- transition.h
+++ transition.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef __TRANSITION_H
 #define __TRANSITION_H
 
@@ -37,43 +36,3 @@ public:
 
 
 #endif // TRANSITION_H_INCLUDED
-=======
-#ifndef __TRANSITION_H
-#define __TRANSITION_H
-
-class State;
-
-class Transition
-{
-private:
-
-    char symbol;
-    int destinationID;
-
-public:
-
-    Transition();
-
-    Transition(char _symbol, int _destinationID);
-
-    Transition(const Transition& toCopy);
-
-    Transition& operator = (const Transition& toCopy);
-
-    bool operator == (const Transition& cmp) const;
-
-    int getDestination() const;
-
-    void setDestination(int);
-
-    char getSymbol() const;
-
-    void print() const;
-
-    bool isEpsilon() const;
-
-};
-
-
-#endif // TRANSITION_H_INCLUDED
->>>>>>> yeah
